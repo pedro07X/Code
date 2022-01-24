033a3615feb4e11baa3d9119b2d536a6cc356e5d
pedro07X <98349017+pedro07X@users.noreply.github.com>
2022-01-24 15:33:36 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var box;
+
+function setup() {
+  createCanvas(800,400);
+  box = createSprite(200,200,50,80);
+  box.shapeColor = "green";
+
+  
+
+}
+
+function draw() {
+  background("black"); 
+  
+if(keyIsDown(RIGHT_ARROW)){
+  box.position.x = box.position.x +5
+}
+if(keyIsDown(LEFT_ARROW)){
+  box.position.x = box.position.x -5
+}
+if(keyIsDown(UP_ARROW)){
+  box.position.y = box.position.y -5
+}
+if(keyIsDown(DOWN_ARROW)){
+  box.position.y = box.position.y +5
+}
+  drawSprites();
+}
